122fba3daa3f66400f953cf59daaa163be2aa2e5
krobnson <krobnson@gmail.com>
2016-08-13 09:19:21 -0700

    rev5

--- scripts/album.js
+++ scripts/album.js
@@ -1,48 +1,48 @@
 // Example Album
 var albumPicasso = {
-   name: 'The Colors',
+   title: 'The Colors',
    artist: 'Pablo Picasso',
    label: 'Cubism',
    year: '1881',
    albumArtUrl: 'assets/images/album_covers/01.png',
    songs: [
-       { name: 'Blue', length: '4:26' },
-       { name: 'Green', length: '3:14' },
-       { name: 'Red', length: '5:01' },
-       { name: 'Pink', length: '3:21'},
-       { name: 'Magenta', length: '2:15'}
+       { title: 'Blue', length: '4:26' },
+       { title: 'Green', length: '3:14' },
+       { title: 'Red', length: '5:01' },
+       { title: 'Pink', length: '3:21'},
+       { title: 'Magenta', length: '2:15'}
    ]
 };
 
 // Another Example Album
 var albumMarconi = {
-   name: 'The Telephone',
+   title: 'The Telephone',
    artist: 'Guglielmo Marconi',
    label: 'EM',
    year: '1909',
    albumArtUrl: 'assets/images/album_covers/20.png',
    songs: [
-       { name: 'Hello, Operator?', length: '1:01' },
-       { name: 'Ring, ring, ring', length: '5:01' },
-       { name: 'Fits in your pocket', length: '3:21'},
-       { name: 'Can you hear me now?', length: '3:14' },
-       { name: 'Wrong phone number', length: '2:15'}
+       { title: 'Hello, Operator?', length: '1:01' },
+       { title: 'Ring, ring, ring', length: '5:01' },
+       { title: 'Fits in your pocket', length: '3:21'},
+       { title: 'Can you hear me now?', length: '3:14' },
+       { title: 'Wrong phone number', length: '2:15'}
    ]
 };
 
 // Third Example Album
 var albumDeadmau5 = {
-   name: 'Deadmau5',
+   title: 'Deadmau5',
    artist: 'Joel Zimmerman',
    label: 'Deadmau5 Records',
    year: '2016',
    albumArtUrl: 'assets/images/album_covers/cheeseHead.png',
    songs: [
-       { name: 'Strobe', length: '4:56' },
-       { name: 'The Veldt', length: '8:42' },
-       { name: 'Ghost N Stuff', length: '6:11'},
-       { name: 'Raise Your Weapon', length: '8:23' },
-       { name: 'Aural Psynapse', length: '7:30'}
+       { title: 'Strobe', length: '4:56' },
+       { title: 'The Veldt', length: '8:42' },
+       { title: 'Ghost N Stuff', length: '6:11'},
+       { title: 'Raise Your Weapon', length: '8:23' },
+       { title: 'Aural Psynapse', length: '7:30'}
    ]
 };
 
@@ -68,7 +68,7 @@ var setCurrentAlbum = function(album) {
   var albumImage = document.getElementsByClassName('album-cover-art')[0];
   var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-   albumTitle.firstChild.nodeValue = album.name;
+   albumTitle.firstChild.nodeValue = album.title;
    albumArtist.firstChild.nodeValue = album.artist;
    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
    albumImage.setAttribute('src', album.albumArtUrl);
