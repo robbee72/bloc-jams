54eba6c426b27963d61eecf65fc90c2b4bd23012
krobnson <krobnson@gmail.com>
2016-08-14 09:15:32 -0700

    github compare

--- scripts/album.js
+++ scripts/album.js
@@ -82,28 +82,34 @@ var getSongItem = function(element) {
         case 'ion-play':
         case 'ion-pause':
             return findParentByClassName(element, 'song-item-number');
+            break;
         case 'album-view-song-item':
             return element.querySelector('.song-item-number');
+            break;
         case 'song-item-title':
         case 'song-item-duration':
             return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+            break;
         case 'song-item-number':
             return element;
+            break;
         default:
             return;
     }  
 };
     var clickHandler = function(targetElement) {
+        
         var songItem = getSongItem(targetElement);  
+        
         if (currentlyPlayingSong === null) {
             songItem.innerHTML = pauseButtonTemplate;
             currentlyPlayingSong = songItem.getAttribute('data-song-number');
-     } else if (currentlyPlayingSong === songItem.getAttribute('data-           song-number')) {
+         } else if (currentlyPlayingSong === songItem.getAttribute('data-           song-number')) {
             songItem.innerHTML = playButtonTemplate;
             currentlyPlayingSong = null;
-      } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+        } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
          var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         currentlyPlayingSongElement.getAttribute('data-song-number');
          songItem.innerHTML = pauseButtonTemplate;
          currentlyPlayingSong = songItem.getAttribute('data-song-number');
      }
