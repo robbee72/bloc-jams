fa7fb6999377cd6a94965ad641c8e3fae1382a26
krobnson <krobnson@gmail.com>
2016-08-19 16:05:37 -0700

    playpausebutton completed

--- scripts/album.js
+++ scripts/album.js
@@ -178,11 +178,26 @@ var previousSong = function() {
 
 };
 
+var togglePlayFromPlayerbar = function() {
+     
+    var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+     if (currentSoundFile.isPaused()) {
+         $currentlyPlayingCell.html(pauseButtonTemplate);
+         $(this).html(playerBarPauseButton);
+         currentSoundFile.play();
+     } else if (currentSoundFile) {
+         $currentlyPlayingCell.html(playButtonTemplate);
+         $(this).html(playerBarPlayButton);
+         currentSoundFile.pause();
+     }
+ };
+ 
+
 var updatePlayerBarSong = function(){
 
-  $('.currently-playing .song-name').text(currentSongFromAlbum.name);
+  $('.currently-playing .song-name').text(currentSongFromAlbum.title);
   $('.currently-playing .artist-name').text(currentAlbum.artist);
-  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.artist);
+  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
   $('.main-controls .play-pause').html(playerBarPauseButton);
 
 };
@@ -200,12 +215,14 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
+  		
 
 $(document).ready( function() {
 
   setCurrentAlbum(albumDeadmau5);
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
-
+  $playPauseButton.click(togglePlayFromPlayerbar);
 });
 
