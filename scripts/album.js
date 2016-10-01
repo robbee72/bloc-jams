df2d564c56e73a649b88a5cca50f62c4ffcd05ee
Keith Robinson <krobnson@gmail.com>
2016-10-01 11:12:34 -0700

    Revert "final update"

--- scripts/album.js
+++ scripts/album.js
@@ -118,6 +118,7 @@ var setCurrentAlbum = function(album) {
     var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     $albumSongList.append($newRow);
   }
+
 };
 
 var setCurrentTimeInPlayerBar = function(currentTime) {
